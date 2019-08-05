f4724fe3dea788858634a46a37dbebfa658bfa6a
Ubuntu <ubuntu@ip-172-31-21-106.us-east-2.compute.internal>
2019-08-05 15:07:12 +0000

    completed assessment week 1

--- js-intro.js
+++ js-intro.js
@@ -4,37 +4,75 @@
 // ---------------------------------------------
 // Consider this variable:
 
-var mantra = "Be the dev"
+    var mantra = "Be the dev"
 
 // 1a. Write the code that returns 'B' from mantra.
-
-// 1b. Write the code that determines if there is an 'x' in mantra.
-
+    function change(yes){
+        return "B"
+    } console.log(change(mantra))
+// // 1b. Write the code that determines if there is an 'x' in mantra.
+    function findX(find){
+        if(find.includes('x')){
+            return 'Mantra includes X'
+        } else {
+            return 'Mantra does not include X'
+        }
+    }
+    console.log(findX(mantra))
 // 1c. Write the code that determines if there is a 'v' in mantra.
-
+    function findV(findit){
+        if(findit.includes('v')){
+            return 'Mantra includes v'
+        } else {
+            return 'Mantra does not include v'
+        }
+    }
+    console.log(findV(mantra))
 // 1d. Stretch: Write a function that can take any letter as an argument and returns if it exists within the given string.
 
 
 // ---------------------------------------------------
 // Consider the following variables:
 
-var myDog = "Snoopy"
-var myCat = "Garfield"
+    var myDog = "Snoopy"
+    var myCat = "Garfield"
 
 // 2a. Create a function that returns the longer of the two variables. Expected output: "Garfield"
-
+    function testLength(str1, str2){
+        if(str1.length > str2.length){
+            return str1
+        } else if (str1.length < str2.length){
+            return str2
+        } else {
+            return 'The strings are equal length'
+        }
+    }
+    console.log(testLength(myDog, myCat))
 // 2b. Create a function that takes in two arguments and returns an array containing the two variables. Expected output: ["Snoopy", "Garfield"]
-
+    function mkArr(string1, string2){
+        let newArr = []
+        newArr.push(string1, string2)
+        return newArr
+    }
+    console.log(mkArr(myDog, myCat))
 // 2c. Stretch: Write a function that returns the two variables in all lowercase letters.
-
-
+    function mkLowerCase(lower){
+        return lower.toLowerCase()
+    }
+    console.log(mkLowerCase(myDog))
+    console.log(mkLowerCase(myCat))
 // -----------------------------------------------
 // Consider this variable:
 
-var myMessage = "Learning is fun!"
+    var myMessage = "Learning is fun!"
 
 // 3a. Write the code that logs each letter of the message using a for loop.
-
+    function loop1(letters){
+        for(let i = 0; i < letters.length; i++){
+            return letters.split('');
+        }
+    }
+    console.log(loop1(myMessage))
 // 4b. Stretch: Write the code that logs each letter of the message using a while loop.
 
 // 4c. Super Stretch: Write the code that logs each letter of the message using forEach().
@@ -43,20 +81,42 @@ var myMessage = "Learning is fun!"
 // ----------------------------------------------
 // Consider the following variable:
 
-var testString = "thisisateststring"
+    var testString = "thisisateststring"
 
 // 5a. Write a function that takes the variable as an argument and returns the string without vowels. Expected output: "thsstststrng"
-
+    function removeVow(string3){
+        return string3.replace(/[aeiou]/g, '');
+    }
+    console.log(removeVow(testString));
 // 5b. Stretch: Update your function to return an error message if the variable is not a string. Copy and paste your function to add these changes.
 
 
 // ----------------------------------------------
 // Consider the following variable:
 
-var toonimals = [ {name: "Itchy", animal: "mouse"}, {name: "Stimpy", animal: "cat"}, {name: "Daffy", animal: "duck"}, {name: "Scratchy", animal: "cat"}, {name: "Ren", animal: "dog"}, {name: "Felix", animal: "cat"}]
+    var toonimals = [ {name: "Itchy", animal: "mouse"}, {name: "Stimpy", animal: "cat"}, {name: "Daffy", animal: "duck"}, {name: "Scratchy", animal: "cat"}, {name: "Ren", animal: "dog"}, {name: "Felix", animal: "cat"}]
 
 // 6a. Write a function that takes in the toonimals array as an argument and returns an array with only the toon objects that are cats. Expected output: [ { name: "Stimpy", animal: "cat" }, { name: "Scratchy", animal: "cat" }, { name: "Felix", animal: "cat" } ]
-
+    // function getCats(kitty) {
+    //     let newKitty = []
+    //     for (let i = 0; i < kitty.length ; ++i){
+    //         if(kitty.animal === "cat"){
+    //             newKitty.push(kitty[i]);
+    //         }
+    //     }
+    //       return newKitty 
+    // }
+    // console.log(getCats(toonimals))
+    function getCats(name1){
+        let newCats = [];
+        for(let i = 0; i < name1.length; i++){
+            if(name1[i] === "cat"){
+                newCats.push(name1[i]);
+            }    
+        }
+        return newCats
+    }
+    console.log(getCats(toonimals))
 //6b. Write a function that returns only the names of all the non-cats. Expected output: "Itchy" "Daffy" "Ren"
 
 //6c. Stretch: Create a function that will print a sentence for every toon stating their name and their animal type. Use string interpolation. Expected output: "Itchy is a mouse" "Stimpy is a cat" "Daffy is a duck" "Scratchy is a cat" "Ren is a dog" "Felix is a cat"
--- js-intro.md
+++ js-intro.md
@@ -5,69 +5,69 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. What is a function? Why would you use one?
 
-  Your answer:
+  Your answer: A function is an action that can be called by using variables. They can be called to perform that action.
 
-  Researched answer:
+  Researched answer: Functions are one of the fundamental building blocks in JavaScript. A function is a JavaScript procedure—a set of statements that performs a task or calculates a value. To use a function, you must define it somewhere in the scope from which you wish to call it.
 
 
 2. What is a method?
 
-  Your answer:
+  Your answer: Methods are like functions, but in objects.
 
-  Researched answer:
+  Researched answer: JavaScript methods are actions that can be performed on objects. A JavaScript method is a property containing a function definition
 
 
 3. List 5 examples of JavaScript string methods.
 
-  Your answer:
+  Your answer: .length, .toUpperCase(), toLowerCase(), concat(), 
 
-  Researched answer:
+  Researched answer: .length, .indexOf(), search(), .leastIndexOf(), slice()
 
 
 4. List 5 examples of JavaScript array methods.
 
-  Your answer:
+  Your answer: .push, .pop, .splice, .split, .join
 
-  Researched answer:
+  Researched answer: toString(), .pop, .push, .join(), shift
 
 
 5. Explain the difference between accessor and mutator methods. Provide an example of each.
 
-  Your answer:
+  Your answer: accessors do not change the original code. mutators do. Accessor: concat Mutator: push
 
-  Researched answer:
+  Researched answer: Accessor is a checker, but Mutator is a changer. 
 
 
 6. Explain the difference between console.log() and return.
 
-  Your answer:
+  Your answer: Return will give you a response from a function, console.log will show the response in terminal.
 
-  Researched answer:
+  Researched answer: return is a keyword that terminates a function and possibly returns a value to the caller of that function. console.log will not influence the flow of your code.
 
 
 7. In regards to functions, what is an argument? What is the difference between an argument and a variable?
 
-  Your answer:
+  Your answer: variables can be used in defferent functions to return different responses. An argument for a function is the specific value that will be passed through
 
-  Researched answer:
+  Researched answer: A parameter is a variable in a method definition. When a method is called, the arguments are the data you pass into the method's parameters. Parameter is variable in the declaration of function. Argument is the actual value of this variable that gets passed to function.
 
 
 8. Give a brief description of JavaScript as if you were explaining it to someone who doesn't know anything about programming.
 
-  Your answer:
+  Your answer: it is a front end language that allows you to view and interact with any applications
 
-  Researched answer:
+  Researched answer:JavaScript is a scripting or programming language that allows you to implement complex things on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc. — you can bet that JavaScript is probably involved. It is the third layer of the layer cake of standard web technologies, two of which (HTML and CSS) we have covered in much more detail in other parts of the Learning Area.
 
 
 9. Give a brief description of proper pair programming techniques. What are the rolls of each person?
 
-  Your answer:
+  Your answer: In pair programmimg, you hav a 'driver' and a 'navigator'. The drive is the person who uses the keyboard and makes the input. The navigator watches and talks with the driver to find solutions and also keeps an eye out for any errors or mistakes that the driver encounters or makes.
 
-  Researched answer:
+  Researched answer: Pair programming is an agile software development technique in which two programmers work together at one workstation. One, the driver, writes code while the other, the observer or navigator, reviews each line of code as it is typed in. The two programmers switch roles frequently.
 
 
 10. What is one thing we did in class this week you found helpful?  
 
-  Your answer:
+  Your answer: the main thing for me would be pair-programming. I am used to working solo, so this is a very important technique for me to learn in ordedr to succeed.
 
-  Researched answer:
+  Researched answer: Pair programming is an agile software development technique in which two programmers work together at one workstation. One, the driver, writes code while the other, the observer or navigator, reviews each line of code as it is typed in. The two programmers switch roles frequently.
